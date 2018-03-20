5fad844adc208976ca09a044fdbcfd15c0103032
tylerbecker <tb95469@ravens.racc.edu>
2018-03-20 07:51:53 -0400

    added loader to the index.html and dynamically display it once it has been loaded. As soon as the page has completed loading in full we removed the div and allow for page interactions.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,75 @@
 // Javascript Document
-    function init() {
 
+window.onload = init();
 
-        window.addEventListener('scroll', function(e) {
-            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-                shrinkOn = 300,
-                header = document.querySelector("header");
-            if (distanceY > shrinkOn) {
-                classie.add(header,"smaller");
-            } else {
-                if (classie.has(header,"smaller")) {
-                    classie.remove(header,"smaller");
-                }
+function init() {
+
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header, "smaller");
+        } else {
+            if (classie.has(header, "smaller")) {
+                classie.remove(header, "smaller");
             }
+        }
+    });
+
+
+    $.ajax({
+        method: 'GET',
+        url: 'assets/data/menu.json',
+        dataType: 'json',
+        success: function (data) {
+
+            var menu = menuBuilder(data.menu);
+
+            $('nav').append(menu)
+
+            $("#loaderDiv").fadeOut("Slow");
+
+        },
+        error: function () {
+
+            console.log('all is not good');
+
+        }
+    });
+
+}
+
+
+function menuBuilder(obj) {
+
+    var theMenu = '';
+
+    if (obj.length > 0) {
+
+        theMenu = theMenu + '<ul>';
+
+        obj.forEach(function (item) {
+
+            theMenu = theMenu + '<li><a href="#">' + item.MenuName + '</a>';
+
+            if (item.Menus.length > 0) {
+
+                theMenu = theMenu + menuBuilder(item.Menus);
+
+            }
+
+            theMenu = theMenu + '</li>';
+
         });
 
+        theMenu = theMenu + '</ul>';
 
-        $.ajax({
-               method: 'GET',
-               url: 'assets/data/menu.json',
-               dataType: 'json',
-               success: function (data) {
+    } else {
 
-                   console.log('all good');
-                   console.log(data.menu.length);
-                   console.log(data.menu);
-
-                   if (data.menu.length > 0) {
-
-                       data.menu.forEach(function (data) {
-
-                           console.log(data.MenuName);
-                           console.log(data.MenuLink);
-
-                           $('nav').append('<a href="' + data.MenuLink + '">' + data.MenuName + '</a>');
-
-                       });
-                   }
-
-               },
-               error: function () {
-                   console.log('all is not good');
-               }
-           });
-
-
-
-       }
-
+        console.log('no data');
 
     }
 
-
-
-
-    window.onload = init();
+    return theMenu;
+}
