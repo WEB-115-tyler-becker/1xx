7f8b0cc0fc6362148c3de4c49769ca05a5504fc5
Flyerstiger <35999707+Flyerstiger@users.noreply.github.com>
2018-03-27 08:52:13 -0400

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,147 @@
 // Javascript Document
-    function init() {
 
 
-        window.addEventListener('scroll', function(e) {
-            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-                shrinkOn = 300,
-                header = document.querySelector("header");
-            if (distanceY > shrinkOn) {
-                classie.add(header,"smaller");
-            } else {
-                if (classie.has(header,"smaller")) {
-                    classie.remove(header,"smaller");
-                }
+
+window.onload = init();
+
+
+
+function init() {
+
+
+
+    window.addEventListener('scroll', function (e) {
+
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+
+            shrinkOn = 300,
+
+            header = document.querySelector("header");
+
+        if (distanceY > shrinkOn) {
+
+            classie.add(header, "smaller");
+
+        } else {
+
+            if (classie.has(header, "smaller")) {
+
+                classie.remove(header, "smaller");
+
             }
+
+        }
+
+    });
+
+
+
+
+
+    $.ajax({
+
+        method: 'GET',
+
+        url: 'me.tylercbecker.com/wp-json/wp-api-menus/v2/menus/2' ,
+
+        dataType: 'json',
+
+        success: function (data) {
+
+
+
+            var menu = menuBuilder(data.menu);
+
+
+
+            $('nav').append(menu)
+
+
+
+            $("#loaderDiv").fadeOut("Slow");
+
+
+
+        },
+
+        error: function () {
+
+
+
+            console.log('all is not good');
+
+
+
+        }
+
+    });
+
+
+
+}
+
+
+
+
+
+function menuBuilder(obj) {
+
+
+
+    var theMenu = '';
+
+
+
+    if (obj.length > 0) {
+
+
+
+        theMenu = theMenu + '<ul>';
+
+
+
+        obj.forEach(function (item) {
+
+
+
+            theMenu = theMenu + '<li><a href="#">' + item.title + '</a>';
+
+
+
+            if (item.children) {
+
+
+
+                theMenu = theMenu + menuBuilder(item.children);
+
+
+
+            }
+
+
+
+            theMenu = theMenu + '</li>';
+
+
+
         });
 
 
-        $.ajax({
-               method: 'GET',
-               url: 'assets/data/menu.json',
-               dataType: 'json',
-               success: function (data) {
 
-                   console.log('all good');
-                   console.log(data.menu.length);
-                   console.log(data.menu);
-
-                   if (data.menu.length > 0) {
-
-                       data.menu.forEach(function (data) {
-
-                           console.log(data.MenuName);
-                           console.log(data.MenuLink);
-
-                           $('nav').append('<a href="' + data.MenuLink + '">' + data.MenuName + '</a>');
-
-                       });
-                   }
-
-               },
-               error: function () {
-                   console.log('all is not good');
-               }
-           });
+        theMenu = theMenu + '</ul>';
 
 
 
-       }
+    } else {
+
+
+
+        console.log('no data');
+
 
 
     }
 
 
 
-
-    window.onload = init();
+    return theMenu;
