9cb1b8e02ec7c7e9df8e218783477d8daf1c247e
Flyerstiger <35999707+Flyerstiger@users.noreply.github.com>
2018-04-03 09:27:07 -0400

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,172 @@
-// Javascript Document
-    function init() {
+// inside out project STEP-105
 
 
-        window.addEventListener('scroll', function(e) {
-            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-                shrinkOn = 300,
-                header = document.querySelector("header");
-            if (distanceY > shrinkOn) {
-                classie.add(header,"smaller");
-            } else {
-                if (classie.has(header,"smaller")) {
-                    classie.remove(header,"smaller");
-                }
+
+window.onload = init();
+
+
+
+function init() {
+
+
+
+    window.addEventListener('scroll', function (e) {
+
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+
+            shrinkOn = 300,
+
+            header = document.querySelector("header");
+
+        if (distanceY > shrinkOn) {
+
+            classie.add(header, "smaller");
+
+        } else {
+
+            if (classie.has(header, "smaller")) {
+
+                classie.remove(header, "smaller");
+
             }
+
+        }
+
+    });
+
+
+
+
+
+    $.ajax({
+
+        method: 'GET',
+
+        url: 'me.tylercbecker.com/wp-json/wp-api-menus/v2/menus/2' ,
+
+        dataType: 'json',
+
+        success: function (data) {
+
+            $('nav').hide();
+
+            var menu = menuBuilder(data.items);
+
+            $('nav').html(menu) slideDown();
+
+            $('nav li a').click(function() {
+                  getPage($(this).data("pgid"));
+            });
+
+            getPage(341);
+
+            $("#loaderDiv").fadeOut("Slow");
+        },
+        error: function () {
+
+
+
+            console.log('all is not good');
+
+
+
+        }
+
+    });
+
+
+
+}
+
+
+
+
+
+function menuBuilder(obj) {
+
+
+
+    var theMenu = '';
+
+
+
+    if (obj.length > 0) {
+
+
+
+        theMenu = theMenu + '<ul>';
+
+
+
+        obj.forEach(function (item) {
+
+
+
+            theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
+
+
+
+            if (item.children) {
+
+
+
+                theMenu = theMenu + menuBuilder(item.children);
+
+
+
+            }
+
+
+
+            theMenu = theMenu + '</li>';
+
+
+
         });
 
 
-        $.ajax({
-               method: 'GET',
-               url: 'assets/data/menu.json',
-               dataType: 'json',
-               success: function (data) {
 
-                   console.log('all good');
-                   console.log(data.menu.length);
-                   console.log(data.menu);
-
-                   if (data.menu.length > 0) {
-
-                       data.menu.forEach(function (data) {
-
-                           console.log(data.MenuName);
-                           console.log(data.MenuLink);
-
-                           $('nav').append('<a href="' + data.MenuLink + '">' + data.MenuName + '</a>');
-
-                       });
-                   }
-
-               },
-               error: function () {
-                   console.log('all is not good');
-               }
-           });
+        theMenu = theMenu + '</ul>';
 
 
 
-       }
+    } else {
+
+
+
+        console.log('no data');
+
 
 
     }
 
 
 
+    return theMenu;
 
-    window.onload = init();
+
+    function getPage(obj) {
+      $("#loaderDiv").fadeIn("slow");
+      $.ajax({
+        method: 'GET',
+        url: 'http://me.tylercbecker.com/wp-json/wp/v2/pages/' + obj,
+        datatype: 'json',
+        success: function(data) {
+            var pgbuild = '';
+            pgbuild = '<section><div class="container">' + data.content.rendered + '</div>'.
+            $("#content").fadeOut(function() {
+              $('html').animate({
+                    scrollTop: 0
+              }, 'slow'); //IE, FF
+              $('body').animate({
+                  scrollTop: 0
+              }, 'slow'); //chrome, don't know if safari works
+              $(this).html(pgbuild).fadeIn();
+              $("#loaderDiv").fadeOut("slow");
+            });
+          },
+          error: function() {
+              console.log('bad');
+        }
+      });
+    }
